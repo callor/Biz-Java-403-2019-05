3c3420f35a5ed15d4bcf1e63ee6417d8746eab58
callor <callor@callor.com>
2019-05-30 16:27:45 +0900

    test

--- Java_02/src/Hello_01.java
+++ Java_02/src/Hello_01.java
@@ -19,6 +19,10 @@ public class Hello_01 {
 		bYes = 10 == 20;
 		
 		System.out.println("Git에 올리기");
+		System.out.println("Git에 올리기");
+		System.out.println("Git에 올리기");
+		System.out.println("Git에 올리기");
+		
 		
 		
 	}
